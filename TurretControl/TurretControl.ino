e51bf36779677810ea78ca85f1e42ab90784c65d
Jackson Romie <jromie0924@gmail.com>
2022-03-09 15:59:55 -0700

    vscoe config and refactoring

--- TurretControl/TurretControl.ino
+++ TurretControl/TurretControl.ino
@@ -1,11 +1,13 @@
-String x;
+String coords;
 void setup() {
  Serial.begin(115200);
  Serial.setTimeout(1);
+
+ pinMode(LED_BUILTIN, OUTPUT);
+ digitalWrite(LED_BUILTIN, LOW);
 }
 void loop() {
  while (!Serial.available());
- x = Serial.readString();
-// Serial.print("(" + x + ")");
-// delay(10);
+ coords = Serial.readString();
+ delay(10);
 }
