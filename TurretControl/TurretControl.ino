e1f23976a3c5176cb5f1d7d43aa398db4fbada42
Jackson Romie <jromie0924@gmail.com>
2022-03-14 22:13:55 -0600

    start working with servo via serial

--- TurretControl/TurretControl.ino
+++ TurretControl/TurretControl.ino
@@ -9,10 +9,16 @@
 #define CENTER 1500
 #define ONE_DEGREE 7
 #define X_PIN 5
+#define PIXELS 320
+#define BUFFER_SIZE 7
 
 Servo xServo;
 
+// FOV: 78 degrees: 39 degrees left or right.
+// Frame: 320 x 320 px
+
 int pos = CENTER;
+char buf[BUFFER_SIZE];
 void setup() {
   xServo.attach(X_PIN, LEFT, RIGHT);
   delay(500);
@@ -22,17 +28,31 @@ void setup() {
 }
 void loop() {
   while (!Serial.available());
-  String coords = Serial.readString();
-  Serial.print(coords);
+//  int x = Serial.readString().toInt();
+  int rlen = Serial.readBytes(buf, BUFFER_SIZE);
+  for (int i = 0; i < rlen; ++i) {
+    Serial.print(buf[i]);
+  }
   Serial.print("\n");
   delay(10);
 
-  Coordinates coordinates = validateString(coords);
-  int x = coordinates.xCoord;
+  int x = atoi(buf);
+
+//  Coordinates coordinates = validateString(coords);
+//  int x = coordinates.xCoord;
+
+  int microseconds = (x / PIXELS) * RIGHT;
+  if(microseconds >= LEFT) {
+//    Serial.print(microseconds);
+    Serial.print("\n");
+    xServoWrite(microseconds);
+  }
+  memset(buf, 0, sizeof buf);
+  delay(100);
 }
 
 Coordinates validateString(String str) {
-  Coordinates coordinates;
+  Coordinates coordinates = Coordinates();
   if (str != NULL) {
     int commaIdx = str.indexOf(",");
     if (commaIdx == -1) {
@@ -50,6 +70,7 @@ Coordinates validateString(String str) {
   return coordinates;
 }
 
-void xServoWrite(int x) {
-  
+void xServoWrite(int ms) {
+  xServo.writeMicroseconds(ms);
+  // delay(10);
 }
